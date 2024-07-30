99f214358018700ad08d20e027c7ece1c19ba947
IsaE7 <bekzatali99@gmail.com>
2024-07-30 19:20:46 +0600

    deploy

--- config.py
+++ config.py
@@ -3,14 +3,16 @@ from os import getenv
 from dotenv import load_dotenv
 from database.database import Database
 
+
 load_dotenv()
-
-token = getenv('TOKEN')
-
-group = getenv('GROUP_TOKEN')
-
-bot = Bot(token=token)
-
-dp = Dispatcher()
-
 database = Database("db.sqlite3")
+debug = getenv("DEBUG", 0)
+if not debug:
+    print("The bot runs on the server")
+    from aiogram.client.session.base import AiohttpSession
+    session = AiohttpSession(proxy=getenv("PROXY"))
+    bot = Bot(token=getenv("TOKEN"), session=session)
+else:
+    print("The bot is running on the computer")
+    bot = Bot(token=getenv("TOKEN"))
+dp = Dispatcher()
